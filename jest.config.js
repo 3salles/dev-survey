dc35b88e633cf53919e389044e51f98317ab433b
3salles <beatrizsallesss@gmail.com>
2021-04-04 20:04:04 -0300

    :wrench: chore: Config Jest.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
+    '<rootDir>/src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
