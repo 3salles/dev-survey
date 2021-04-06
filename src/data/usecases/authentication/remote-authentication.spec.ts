d33e1039d250310f17a755913c8b75a3f8deb448
3salles <beatrizsallesss@gmail.com>
2021-04-06 10:04:43 -0300

    :white_check_mark: test: Should return an AccountModel if HttPostClient returns 200.

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,11 +4,11 @@ import faker from 'faker'
 import { RemoteAutentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "@/data/test/mock-http-client"
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpect-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { AccountModel } from '@/domain/models/account-model'
+import { mockAuthentication, mockAccountModel } from '@/domain/test/mock-account'
 
 
 type SutTypes = {
@@ -80,6 +80,20 @@ describe('RemoteAuthentication', () => {
         }
         const promise = sut.auth(mockAuthentication())
         await expect(promise).rejects.toThrow(new UnexpectedError())
+      })
+  })
+
+  describe('Success', () => {
+    test('Should return an AccountModel if HttPostClient returns 200',
+      async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const httpResult = mockAccountModel()
+        httpPostClientSpy.response = {
+          statusCode: HttpStatusCode.ok,
+          body: httpResult
+        }
+        const account = await sut.auth(mockAuthentication())
+        await expect(account).toEqual(httpResult)
       })
   })
 
--- src/domain/test/mock-authentication.ts
+++
@@ -1,7 +0,0 @@
-import faker from 'faker'
-import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
-
-export const mockAuthentication = (): AuthenticationParams => ({
-  email: faker.internet.email(),
-  password: faker.internet.password()
-})
